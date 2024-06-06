fbfec2daece1e756a1f25aaa9f1f5d186eede5d1
TA GIA BAO <git@github.com>
2024-06-06 21:36:24 +0700

    merge

--- react-search-bar/src/App.jsx
+++ react-search-bar/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import './App.css'
-import logo from './geekup.png';
 import { SearchBar } from './components/SearchBar';
 import { SearchResultsList } from "./components/SearchResultsList";
 
